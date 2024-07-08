4bfcd4bbf5a4995c8866e1050957f97fdd6e708a
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-08 14:00:28 -0300

    item entity

---
+++ src/main/java/andersonfflores/carrinhoproduto/Models/Item.java
@@ -0,0 +1,77 @@
+package andersonfflores.carrinhoproduto.Models;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class Item {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    private Carrinho carrinho;
+
+    @OneToOne
+    private Produto produto;
+
+    private int quantidade;
+
+    private BigDecimal valorTotal;
+
+    public Item() {
+    }
+
+    public Item(UUID id, Carrinho carrinho, Produto produto, int quantidade, BigDecimal valorTotal) {
+        this.id = id;
+        this.carrinho = carrinho;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.valorTotal = valorTotal;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Carrinho getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(Carrinho carrinho) {
+        this.carrinho = carrinho;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
