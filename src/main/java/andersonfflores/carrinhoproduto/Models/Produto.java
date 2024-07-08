3c410a88a916c459cf73d3fde9d8102b4a7a71ae
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-08 14:00:42 -0300

    produto entity

---
+++ src/main/java/andersonfflores/carrinhoproduto/Models/Produto.java
@@ -0,0 +1,62 @@
+package andersonfflores.carrinhoproduto.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class Produto {
+
+    @Id
+    private UUID id;
+
+    private String nome;
+
+    private String descricao;
+
+    private BigDecimal precoUnitario;
+
+    public Produto() {
+    }
+
+    public Produto(UUID id, String nome, String descricao, BigDecimal precoUnitario) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
