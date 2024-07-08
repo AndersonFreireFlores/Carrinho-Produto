0848d0ee8640ecf659ad9f97c5f6de4df8e370a9
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-08 15:36:45 -0300

    not using flyway anymore

--- src/main/resources/db/migration/V1__create_table_produto.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE table produto
-(
-    id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    nome          VARCHAR(100) NOT NULL,
-    descricao     VARCHAR(250) NOT NULL,
-    precoUnitario money        NOT NULL
-);
--- src/main/resources/db/migration/V2__create_table_carrinho.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE table carrinho
-(
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    preco_total money NOT NULL
-)
