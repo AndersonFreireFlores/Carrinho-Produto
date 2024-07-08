121a6c86605236e9c002bb5ae029c08c28e58c21
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-07-08 13:15:01 -0300

    first commit

---
+++ src/main/java/andersonfflores/carrinhoproduto/CarrinhoProdutoApplication.java
@@ -0,0 +1,13 @@
+package andersonfflores.carrinhoproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andersonfflores/carrinhoproduto/CarrinhoProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package andersonfflores.carrinhoproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
